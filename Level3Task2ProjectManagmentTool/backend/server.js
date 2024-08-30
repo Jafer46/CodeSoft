2785b2917416c2554a84d77e555dee473ffc29d8
Jafer46 <123theelite@gmail.com>
2024-08-30 10:42:10 -0700

    changing Config to config

--- Level3Task2ProjectManagmentTool/backend/server.js
+++ Level3Task2ProjectManagmentTool/backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-const dbconnection = require('./Config/dbConnection')
+const dbconnection = require('./config/dbConnection')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const errorHandler = require('./middlewares/errorHandling')
